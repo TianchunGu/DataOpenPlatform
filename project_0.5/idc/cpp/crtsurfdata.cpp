ae1b6af261bf013b6aef20f69531dc3d2a619ad2
TianchunGu <96930846+TianchunGu@users.noreply.github.com>
2025-06-05 17:32:48 +0800

    feat(project_0.5): 实现 crtsurfdata 程序加载站点参数功能
    - 添加站点参数结构体和加载站点参数的函数
    - 实现从站点参数文件中读取数据并存储到容器中
    - 为生成气象站点数据和保存到文件中做准备

--- project_0.5/idc/cpp/crtsurfdata.cpp
+++ project_0.5/idc/cpp/crtsurfdata.cpp
@@ -7,6 +7,21 @@
 #include "_public.h"
 using namespace idc;
 
+// 省   站号  站名 纬度   经度  海拔高度
+//  安徽,58015,砀山,34.27,116.2,44.2
+struct st_stcode {  // 站点参数结构体
+  // string 操作方便、自动扩容 操作数据库时 string没有优势，所以用char[]
+  char provname[31];  // 省
+  char obtid[11];     // 站号
+  char obtname[31];   // 站名
+  double lat;         // 纬度
+  double lon;         // 经度
+  double height;      // 海拔高度
+};
+
+list<st_stcode> stlist;                  // 存放全部的站点参数
+bool loadstcode(const string& inifile);  // 把站点参数文件加载到stlist容器中
+
 clogfile logfile;  // 本程序运行的日志
 
 // 程序退出和信号2,15的处理函数
@@ -46,9 +61,13 @@ int main(int argc, char* argv[]) {
   // 记录程序开始运行的日志
   logfile.write("crtsurfdata 开始运行\n");
 
-  // 在这里编写处理业务的代码
   // TODO: 实现生成气象站点数据的逻辑
-  sleep(100);
+  // 1. 从站点参数文件中加载站点参数，存放于容器中；
+  if (loadstcode(argv[1]) == false) {
+    EXIT(-1);
+  }
+  // 2. 根据站点参数，生成站点观测数据（随机数）；
+  // 3. 把站点观测数据保存到文件中。
 
   // 记录程序运行结束的日志
   logfile.write("crtsurfdata 运行结束\n");
@@ -63,3 +82,46 @@ void EXIT(int sig) {
 
   exit(0);
 }
+
+// 把站点参数文件加载到stlist容器中
+bool loadstcode(const string& inifile) {
+  cifile ifile;  // 读取文件的对象
+  if (ifile.open(inifile) == false) {
+    logfile.write("ifile.open(%s) failed.\n", inifile.c_str());
+    return false;
+  }
+  string strbuffer;           // 存放从文件中读取的每一行。
+  ifile.readline(strbuffer);  // 读取站点参数文件的第一行,他是标题，扔掉
+  ccmdstr cmdstr;             // 用于拆分从文件中读取的行。
+  st_stcode stcode;           // 站点参数的结构体
+
+  while (ifile.readline(strbuffer)) {
+    // logfile.write("strbuffer=%s\n",
+    // strbuffer.c_str());//输出读取的行到日志文件。
+    // 拆分从文件中读取的行，例如：安徽,58015,砀山,34.27,116.2,44.2
+    cmdstr.splittocmd(strbuffer, ",");             // 拆分字符串
+    memset(&stcode, 0, sizeof(struct st_stcode));  // 初始化结构体
+
+    cmdstr.getvalue(0, stcode.provname, 30);  // 省
+    cmdstr.getvalue(1, stcode.obtid, 10);     // 站点代码
+    cmdstr.getvalue(2, stcode.obtname, 30);   // 站点名称
+    cmdstr.getvalue(3, stcode.lat);           // 纬度
+    cmdstr.getvalue(4, stcode.lon);           // 经度
+    cmdstr.getvalue(5, stcode.height);        // 海拔高度
+
+    stlist.push_back(stcode);  // 将站点数据添加到列表中
+  }
+  // 这里不需要手工关闭文件，cifile析构函数会自动关闭文件。
+
+  // 把容器中全部数据写入日志中。
+  // logfile.write("站点数:%d\n", stlist.size());
+  // for (auto& aa : stlist) {
+  //   logfile.write("%s %s %s %.2f %.2f %.2f\n\n", aa.provname, aa.obtname,
+  //                 aa.obtid, aa.lat, aa.lon, aa.height);
+  // }
+  return true;
+}
+
+/*
+./crtsurfdata ../ini/stcode.ini outpath logfile
+*/
